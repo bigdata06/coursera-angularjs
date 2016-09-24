29596e0efb182ee0e67cc83ef5c3dec67f2d736b
Xavier BOUCLET <mikrethor@gmail.com>
2016-09-24 07:52:37 -0400

    Final solution to test

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,19 +1,21 @@
 (function () {
 'use strict';
-
+//Module declaration
 angular.module('ShoppingListCheckOff', [])
 .controller('ToBuyShoppingController', ToBuyShoppingController)
 .controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
 .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
-function ToBuyShoppingController(ShoppingListCheckOffService) {
+//Controller for the elements to buy
+ToBuyShoppingController.$inject = ['ShoppingListCheckOffService','$scope'];
+function ToBuyShoppingController(ShoppingListCheckOffService,$scope) {
   var toBuyList = this;
-  ShoppingListCheckOffService.addItem('cookies',1);
-  ShoppingListCheckOffService.addItem('coke',2);
-  ShoppingListCheckOffService.addItem('a',3);
-  ShoppingListCheckOffService.addItem('a',4);
-  ShoppingListCheckOffService.addItem('s',5);
+  //init elements
+  ShoppingListCheckOffService.addItem('cookies',10);
+  ShoppingListCheckOffService.addItem('coke',5);
+  ShoppingListCheckOffService.addItem('cake',2);
+  ShoppingListCheckOffService.addItem('cheese',1);
+  ShoppingListCheckOffService.addItem('ham',5);
 
   toBuyList.toBuyItems = ShoppingListCheckOffService.getToBuyItems();
 
@@ -24,21 +26,22 @@ function ToBuyShoppingController(ShoppingListCheckOffService) {
 
 }
 
-
+//Controller for the elements bought
 AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
 function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
   var boughtList = this;
-  boughtList.boughtItems = ShoppingListCheckOffService.getBoughtItems();
+ boughtList.boughtItems = ShoppingListCheckOffService.getBoughtItems();
 }
 
-
+//Service to share element between controllers
 function ShoppingListCheckOffService() {
   var service = this;
-
   // List of shopping items
   var toBuyItems = [];
+  // List of bought items
   var boughtItems = [];
 
+//add item for init purpose
   service.addItem = function (itemName, quantity) {
     var item = {
       name: itemName,
@@ -47,19 +50,18 @@ function ShoppingListCheckOffService() {
     toBuyItems.push(item);
   };
 
+  //Transfer method between list
   service.buyItem = function (itemIndex) {
-    console.log("item : "+itemIndex);
     boughtItems.push(toBuyItems[itemIndex]);
-    console.log("after push");
     toBuyItems.splice(itemIndex, 1);
-    console.log("splice");
-    itemIndex--;
   };
 
+  //getter for tobuy elements list
   service.getToBuyItems = function () {
     return toBuyItems;
   };
 
+  //getter for bought elements list
   service.getBoughtItems = function () {
     return boughtItems;
   };
